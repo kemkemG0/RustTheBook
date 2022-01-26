8f656eb4ea1cf7d2e8da35fca5ec44f07d57ec86
kemkemG0 <kenshin.jpsdeveloper@gmail.com>
2022-01-26 15:21:44 -0800

    separate files into modules

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,37 +1,13 @@
+use http::{
+    Request,
+    Method
+};
+use server::Server;
+
+mod http;
+mod server;
 fn main() {
     let string = String::from("localhost:8080".to_string());
     let server = Server::new(string);
     server.run();
 }
-struct  Server{
-    addr:String,
-}
-impl Server{
-    fn new(addr:String)->Self{
-        Self { 
-            addr
-        }
-    }
-    fn run(self){
-        println!("Listening on {}",self.addr);
-    }
-}
-
-struct Request{
-    path:String,
-    query_string:Option<String>,
-    method:Method
-}
-enum Method{
-    GET,
-    POST,
-    DELETE,
-    PUT
-}
-
-/*
-GET /user?id=10 HTTP/1.1\r\n
-HEADERS\r\n
-BODY
-*/
-
