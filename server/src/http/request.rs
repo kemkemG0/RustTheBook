a295595d687a86d5f99c38edc370ad291bf41358
kemkemG0 <kenshin.jpsdeveloper@gmail.com>
2022-01-29 19:02:15 -0800

    custom error type

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,5 +1,7 @@
 use super::method::Method;
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Display, Debug, Formatter, Result as FmtResult};
 pub struct Request{
     path:String,
     query_string:Option<String>,
@@ -7,10 +9,37 @@ pub struct Request{
 }
 
 impl TryFrom<&[u8]> for Request{
-    type Error = String;
+    type Error = ParseError;
 
     // GET /search?name=abc&sort=1 HTTP/1.1
     fn try_from(value: &[u8]) -> Result<Self, Self::Error>{
         unimplemented!()
     }
 }
+pub enum ParseError{
+    InvalidRequest,
+    InvalidEncodeing,
+    InvalidProtocl,
+    InvalidMethod
+}
+impl ParseError{
+    fn message(&self)->&str{
+        match self{
+            Self::InvalidRequest =>"InvalidRequest",
+            Self::InvalidEncodeing => "InvalidEncodeing",
+            Self::InvalidProtocl => "InvalidProtocl",
+            Self::InvalidMethod =>"InvalidMethod"
+        }
+    }
+}
+impl Display for ParseError{
+    fn fmt(&self, f:&mut Formatter)->FmtResult{
+        write!(f,"{}",self.message())
+    }
+}
+impl Debug for ParseError{
+    fn fmt(&self, f:&mut Formatter)->FmtResult{
+        write!(f,"{}",self.message())
+    }
+}
+impl Error for ParseError{}
